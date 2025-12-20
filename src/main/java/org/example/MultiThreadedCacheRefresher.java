ce6a92eb6bb557bc6d2ab56cfc202a3e2d8c77d8
Indukuru, Ajay <ajaycindukuru@gmail.com>
2025-12-19 20:20:39 -0500

    12/19/2025

---
+++ src/main/java/org/example/MultiThreadedCacheRefresher.java
@@ -0,0 +1,71 @@
+package org.example;
+
+import org.example.helper.LoggingRefreshListener;
+import org.example.helper.ProductService;
+import org.example.helper.RefreshListener;
+import org.example.model.ProductInfo;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MultiThreadedCacheRefresher {
+
+    static Map<String, ProductInfo> products = new ConcurrentHashMap<>();
+    static ProductService productService = new ProductService();
+    static ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    static ExecutorService reader = Executors.newFixedThreadPool(5);
+    static RefreshListener refreshListener= new LoggingRefreshListener();
+
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            try {
+                addProducts();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+
+        scheduler.scheduleAtFixedRate(() -> {
+            var allProducts = productService.getProducts();
+            AtomicLong refreshedCount = new AtomicLong();
+            for (ProductInfo product: allProducts) {
+                products.compute(product.getProductName(), (k, v) -> {
+                    refreshedCount.getAndIncrement(); return product;});
+            }
+            products.values().forEach(System.out::println);
+            refreshListener.onRefreshComplete(refreshedCount.get());
+        }, 0, 10, TimeUnit.SECONDS);
+
+        var nowPlus10 = LocalDateTime.now().plusSeconds(10);
+        while (nowPlus10.isAfter(LocalDateTime.now())) {
+            for (int i=0; i<=5; i++) {
+                reader.submit(() -> {
+                    var apple = products.get("Apple iPhone");
+                    var samsung = products.get("Samsung Galaxy");
+                    System.out.println(apple.toString());
+                    System.out.println(samsung.toString());
+                });
+                Thread.sleep(2000);
+            }
+        }
+
+        reader.shutdown();
+        scheduler.shutdown();
+        reader.awaitTermination(1, TimeUnit.MINUTES);
+        scheduler.awaitTermination(1, TimeUnit.MINUTES);
+    }
+
+    private static void addProducts() throws InterruptedException {
+        productService.addProduct(new ProductInfo(1, "Apple iPhone", new AtomicInteger(100), 1400));
+        productService.addProduct(new ProductInfo(2, "Samsung Galaxy", new AtomicInteger(100), 900));
+        Thread.sleep(2000);
+        productService.updateProductPrice(1, new AtomicInteger(90));
+        Thread.sleep(2000);
+        productService.updateProductPrice(2, new AtomicInteger(75));
+        productService.updateProductPrice(1, new AtomicInteger(50));
+    }
+}
